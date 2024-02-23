aa2408c9ddea10c715e725d4d2511d348c5bf061
Rajmohinder kaur <Rajmohinder kaur@DESKTOP-R5PRAV4>
2024-02-23 10:16:28 -0500

    Midterm exam

--- src/days/Days.java
+++ src/days/Days.java
@@ -17,6 +17,16 @@ import java.util.Scanner;
  * @author sivagamasrinivasan
  jn 22nd
  */
+
+enum Day {
+    SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+}
+class Weekdays {
+    public void nameOfDay(Day day) {
+        System.out.println("Day name: " + day);
+    }
+}
+
 public class Days {
 
     /**
@@ -26,10 +36,18 @@ public class Days {
          
     Scanner in =new Scanner(System.in);
     System.out.println("Enter the day number (one,two,three,four,five) in string");
-    String code = in.next();
-    Weekdays t= new Weekdays();
-    t.nameOfDay(code); 
-    }// TODO code application logic here
-    
+    int dayNumber = in.nextInt();
+
+        // Ensure the entered number is within the valid range
+        if (dayNumber >= 0 && dayNumber <= 6) {
+            // Convert the entered number to the corresponding Day enum
+            Day selectedDay = Day.values()[dayNumber];
+
+            Weekdays weekdays = new Weekdays();
+            weekdays.nameOfDay(selectedDay);
+        } else {
+            System.out.println("Invalid day number. Please enter a number between 0 and 6.");
+        }
+    }
     
 }
